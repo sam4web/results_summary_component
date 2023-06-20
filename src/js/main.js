1a80ea033cbbac127f4e909aa152a21b2163b3f9
Sijal Manandhar <sijal.m06@gmail.com>
2023-06-20 13:38:06 +0545

    removed comments in js

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,3 @@
-/*
-<div class='flex items-center bg-orangey-yellow p-4 rounded-lg'>
-  <div class='flex flex-1 items-center'>
-    <img
-      src='./assets/icon-memory.svg'
-      class='w-[24px] h-[24px]'
-      alt='icon-memory'
-    />
-    <p class='font-medium tracking-wide font-hanken text-orangey-yellow text-lg ml-2'>
-      Memory
-    </p>
-  </div>
-  <p class='font-bold tracking-wide font-hanken text-lg text-light-lavender'>
-    <span class='text-dark-gray-blue'>92 </span> / 100
-  </p>
-</div>;
-*/
-
 const statsItem = ({ category, color, score, icon }) =>
   `<div class='flex items-center bg-${color} p-4 rounded-lg'>
     <div class='flex flex-1 items-center'>
